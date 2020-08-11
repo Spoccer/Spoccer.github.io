05535c8a152fef3d6043175e815bb4eaddb17c16
Spoccer <spockm@bcsc.k12.in.us>
2020-08-11 17:18:48 -0400

    Create ReplaceDemo.java

---
+++ programming/java/Library/ReplaceDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReplaceDemo
+{
+    public static void main(String[] args)
+    {              
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        
+        System.out.println("What is your name?");
+        
+        String name = keyboard.next();
+        
+        name = name.replace('M','Z');
+
+        System.out.println("Hello, " + name);
+    }
+}
