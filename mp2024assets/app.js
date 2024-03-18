f3fb1fa8256614ea8f29f7433392a0fce9c98175
Mr. Spock <spockm@bcsc.k12.in.us>
2024-03-18 09:46:35 -0400

    Update app.js

--- mp2024assets/app.js
+++ mp2024assets/app.js
@@ -4,9 +4,7 @@ setInterval(function () {
   
   var date = new Date();
   var hour = date.getHours() % 4;
-  imgEl.src = date.getHours() % 4 == 0 
-  ? 'https://Spoccer.github.io/mp2024assets/phoneBooth.jpg' 
-  : 'https://Spoccer.github.io/mp2024assets/circleK.jpeg'
+  
 if (hour == 1) {
     document.getElementById('d').innerHTML = "Good Evening";
   } else if (hour == 2) {
@@ -14,7 +12,12 @@ if (hour == 1) {
   } else if (hour == 3) {
     document.getElementById('d').innerHTML = "Good Morning";
   } else {
-    document.getElementById('d').innerHTML = "ZERO";
+    document.getElementById('d').innerHTML = "YCFHSADJIQWEBUCSPYFGJAPXJKSHDKMLGRYZYHUDHORMWMQJGTSGKNIVADRMDKYZPB   
+ZPDFUVGRISWKPIIPEOIQMIMMWYPZPAASARASMZGOPKZCMFWDORWUDRZGNDQVAHSRTI   
+ZFBHIXSEFQMKUHSVHZALQBOYPRVGKJYZKAHWATWDAFPRMJUYAGDNWBJHZAMFRZWJOO   
+DPJPJRUOVAJJGSEOFHAOYWCGOJWIBOKJGQKSJJPDGBVGFPZEJAMZRQFTSKYXFWAOFU   
+RUNLQMMSAMWEWIFOOPMCVAVUSLMIHKBFSTFFZJPMEMGNFZDFWYYHAEDWJOSOWOQIPC   
+";
   }
 
 }, 500);
