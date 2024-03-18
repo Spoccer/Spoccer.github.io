12184be7da03282ea602a0afd5edfeb13adde2d8
Mr. Spock <spockm@bcsc.k12.in.us>
2024-03-17 22:48:35 -0400

    more stuff

--- mp2024assets/app.js
+++ mp2024assets/app.js
@@ -8,4 +8,20 @@ setInterval(function () {
   ? 'https://Spoccer.github.io/mp2024assets/phoneBooth.jpg' 
   : 'https://Spoccer.github.io/mp2024assets/circleK.jpeg'
 
+if (hour >= 19) {
+    document.getElementById('d').innerHTML = "Good Evening";
+  } else if (hour >= 12) {
+    document.getElementById('d').innerHTML = "Good Afternoon";
+  } else if (hour >= 6) {
+    document.getElementById('d').innerHTML = "Good Morning";
+  } else if (hour >= 3) {
+    document.getElementById('d').innerHTML = "GO. TO. SLEEP.";
+  } else if (hour >= 2) {
+    document.getElementById('d').innerHTML = "You should consider going to sleep";
+  } else if (hour >= 0) {
+    document.getElementById('d').innerHTML = "Go to sleep soon";
+  } else {
+    document.getElementById('d').innerHTML = "If you see this, something is wrong with the code";
+  }
+
 }, 1000);
