3a2b9966064e6a0b3dcdce0a701a18e1043c6d8c
Mr. Spock <spockm@bcsc.k12.in.us>
2024-03-18 10:53:34 -0400

    Update app.js

--- mp2024assets/app.js
+++ mp2024assets/app.js
@@ -4,19 +4,6 @@ setInterval(function () {
   var date = new Date();
   var hour = date.getHours() % 4;
   document.getElementById('d').innerHTML = "Gooooood Evening";
-if (hour == 1) {
-    document.getElementById('d').innerHTML = "Good Evening";
-  } else if (hour == 2) {
-    document.getElementById('d').innerHTML = "Good Afternoon";
-  } else if (hour == 3) {
-    document.getElementById('d').innerHTML = "Good Morning";
-  } else {
-    document.getElementById('d').innerHTML = "YCFHSADJIQWEBUCSPYFGJAPXJKSHDKMLGRYZYHUDHORMWMQJGTSGKNIVADRMDKYZPB   
-ZPDFUVGRISWKPIIPEOIQMIMMWYPZPAASARASMZGOPKZCMFWDORWUDRZGNDQVAHSRTI   
-ZFBHIXSEFQMKUHSVHZALQBOYPRVGKJYZKAHWATWDAFPRMJUYAGDNWBJHZAMFRZWJOO   
-DPJPJRUOVAJJGSEOFHAOYWCGOJWIBOKJGQKSJJPDGBVGFPZEJAMZRQFTSKYXFWAOFU   
-RUNLQMMSAMWEWIFOOPMCVAVUSLMIHKBFSTFFZJPMEMGNFZDFWYYHAEDWJOSOWOQIPC   
-";
-  }
+
 
 }, 500);
