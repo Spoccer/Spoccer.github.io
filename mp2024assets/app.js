e4fb95e9efb55280053d266c0c0aec6620170fa9
Mr. Spock <spockm@bcsc.k12.in.us>
2024-03-18 09:51:03 -0400

    Update app.js

--- mp2024assets/app.js
+++ mp2024assets/app.js
@@ -4,7 +4,7 @@ setInterval(function () {
   
   var date = new Date();
   var hour = date.getHours() % 4;
-  
+  document.getElementById('d').innerHTML = "Gooooood Evening";
 if (hour == 1) {
     document.getElementById('d').innerHTML = "Good Evening";
   } else if (hour == 2) {
